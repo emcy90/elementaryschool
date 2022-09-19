b1e17f955fea5ca41ab2d9a8980e195249e1a730
Emcy90 <emma@emcy.se>
2022-09-19 13:50:13 +0200

    added attributes for student

--- student.py
+++ student.py
@@ -4,8 +4,13 @@ from person import Person
 
 class Student(Person):
     """class student that inherits from class person attributes for person_id, name and birthday"""
-    def __init__(self, person_id, name, birthday):
+    def __init__(self, person_id, name, birthday, schedule, busses, classroom, course, grade):
         super().__init__(person_id=person_id, name=name, birthday=birthday)
+        self.schedule = schedule
+        self.busses = busses
+        self.classroom = classroom
+        self.course = course
+        self.grade = grade
 
     def __str__(self):
         return self.short_string()
