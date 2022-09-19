ac3cb73b195d5b29852ba84b247ea41cb1911a07
Emcy90 <emma@emcy.se>
2022-09-19 11:38:59 +0200

    added attributes to person and student classes

--- Person.py
+++ Person.py
@@ -1,5 +1,10 @@
-class Person():
-    #class for mutual attributs shared by admin, teacher and student
-    id = ""
-    name = ""
-    birthday = ""
+"""
+Class for persons at school which includes but not limited to teachers, administrators and students
+"""
+
+class Person:
+    """class with attributes person_id and name and birthday which is shared by all"""
+    def __init__(self, person_id, name, birthday):
+        self.person_id = person_id
+        self.name = name
+        self.birthday = birthday
--- Student.py
+++ Student.py
@@ -1,3 +1,5 @@
 import Person
 
 class Student(Person):
+    def __init__(self, person_id, name, birthday):
+        super().__init__(person_id=person_id, name=name, birthday=birthday)
