c699bda6eb762f62165d94485bf2615547b00f25
Emcy90 <emma@emcy.se>
2022-09-21 09:27:59 +0200

    minor modification to course to remove argument and shorten an f-string

--- course.py
+++ course.py
@@ -9,12 +9,11 @@ class Course:
     attributes from which are subject_time and subject_name
     as all subjects require that
     """
-    def __init__(self, course_time, course_name, course_plan, course_grading, course_schedule):
+    def __init__(self, course_time, course_name, course_plan, course_grading):
         self.course_time = course_time
         self.course_name = course_name
         self.course_plan = course_plan
         self.course_grading = course_grading
-        self.course_schedule = course_schedule
 
     def __repr__(self):
         return f"Course(name='{self.course_name}', time='{self.course_time}')"
@@ -31,5 +30,5 @@ class Course:
 
     def long_string(self) -> str:
         """function to return course-name as string"""
-        return f"Course name {self.course_name}, time {self.course_time}, plan {self.course_plan}," \
-               f" grading {self.course_grading} , schedule {self.course_schedule} "
+        return f"Course name {self.course_name}, time {self.course_time}, plan" \
+               f" {self.course_plan}, grading {self.course_grading}"
