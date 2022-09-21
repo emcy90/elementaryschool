a2b2f2e29b61e74597e0e8de3bd807eb0a5842e8
Emcy90 <emma@emcy.se>
2022-09-21 09:27:59 +0200

    minor modification to course to remove argument and shorten an f-string

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-# This is a sample Python script.
+import time
 
-# Press Skift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def clock(n):
+    """ this function creat a list of integer
+    from 0 -> n after converting them to string
+    as: [o, 1, 2......n] """
+    return list(map(str, range(n)))
+
+
+# start
+t_clock_start = time.time()
+# call the function
+clock_time = clock(n=10000)
+# end time
+t_clock_end = time.time()
+# print the result
+modern_fun_exe_time = t_clock_end - t_clock_start
+print(f'Execution time for old fashion = {modern_fun_exe_time}')
 
 
 def print_hi(name):
